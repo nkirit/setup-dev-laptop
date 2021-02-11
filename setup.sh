1e3d6b96b374c2310b87657a47bd7e586918abda
Neel Kirit <neelkirit@neels-mbp.lan>
2021-02-11 14:08:17 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,37 @@
+# Install Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap homebrew/cask-versions
+brew update
+brew tap homebrew/cask
+echo " Homebrew Installed yay 🎉"
+# Install Git
+brew install git
+echo " Git Installed yay 🎉"
+# Install Python3
+# brew install python
+echo " Python3 Installed yay 🎉"
+# Install sublime-text
+# brew install --cask sublime-text
+echo " Sublime Text Installed yay 🎉"
+# Install Docker
+brew install docker
+echo " Docker Installed yay 🎉"
+docker --version
+# Install Java
+brew install java
+echo " Java Installed yay 🎉"
+# Install Maven
+brew install maven
+echo " Maven Installed yay 🎉"
+# Install Pycharm
+brew install --cask pycharm
+echo " Pycharm Installed yay 🎉"
+# Install Chrome
+brew install --cask google-chrome
+echo " Chrome Installed yay 🎉"
+# Install Slack
+brew install --cask slack
+echo " Slack Installed yay 🎉"
+# Install Node
+brew install node
+echo " Node Installed yay 🎉"
